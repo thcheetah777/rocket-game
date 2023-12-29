686c6e6f23c6bbf50fad3e295ff3016bb2281cb9
Leo Feng <thcheetah777@gmail.com>
2023-12-29 12:53:00 +0100

    🖼️ backgrounds and polish!

--- SceneManager.gd
+++ SceneManager.gd
@@ -1,6 +1,10 @@
 extends Node
 
 var level_1 = preload("res://levels/level_1.tscn")
+var level_2 = preload("res://levels/level_2.tscn")
+
+func next_level():
+	pass
 
 func change_scene(scene: PackedScene):
 	pass
---
+++ camera.gd
@@ -0,0 +1,7 @@
+extends Camera2D
+class_name Camera
+
+@export var tilt_magnitude = 0.3
+
+func _process(delta: float) -> void:
+	rotation_degrees = -(Globals.player.position - position).x * 0.01 * tilt_magnitude
